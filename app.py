5522839cc6f05708305bfbe99f4cd271fd400803
Harshavardhan Reddy <151720877+bharshavardhanreddy924@users.noreply.github.com>
2025-03-24 12:56:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -725,5 +725,5 @@ def handle_standalone_mode():
         session['standalone'] = True
         g.standalone = True
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
